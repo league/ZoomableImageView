8e0e8a0ed11a99510680d5aff1d3f70a327bcfed
Christopher League <league@contrapunctus.net>
2013-08-01 14:23:24 -0400

    Modify `nomaven` example to simulate loading image from web.
    We first display a low-resolution image, then some seconds later
    replace it with the high-resolution version of the same image.

--- zoomableimageview_examples/nomaven/src/fr/jmartinez/zoomable/MainActivity.java
+++ zoomableimageview_examples/nomaven/src/fr/jmartinez/zoomable/MainActivity.java
@@ -2,13 +2,38 @@ package fr.jmartinez.zoomable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Runnable {
+
+    ImageView imageView;
+    TextView textView;
+    int countdown = 8;
+    Handler handler;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.view);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(Integer.toString(countdown));
+        // Simulate a slow load from the web, using a countdown timer.
+        handler = new Handler();
+        handler.postDelayed(this, 1000);
 	}
 
+    @Override
+    public void run() {
+        countdown--;
+        textView.setText(Integer.toString(countdown));
+        if(countdown > 0) {
+            handler.postDelayed(this, 1000);
+        } else {
+            // Finally the image arrived!
+            imageView.setImageResource(R.drawable.screenshot);
+        }
+    }
 }
